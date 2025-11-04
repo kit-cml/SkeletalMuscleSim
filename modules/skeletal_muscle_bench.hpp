4d0a7802fc2b712f215c1be5c15b9b5a81c473fb
vondarkness <aroli.marcellinus@gmail.com>
2025-11-04 13:43:47 +0900

    first commit

---
+++ modules/skeletal_muscle_bench.hpp
@@ -0,0 +1,11 @@
+#ifndef SKELETAL_MUSCLE_BENCH_HPP
+#define SKELETAL_MUSCLE_BENCH_CPP
+
+#include <types/parameter.hpp>
+
+#include <cstdio>
+
+int skeletal_muscle_bench(const Parameter *p_param);
+void end_of_cycle_funct(short *pace_count);
+
+#endif
